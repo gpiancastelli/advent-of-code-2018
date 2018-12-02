88b2d57d2f2422d5a5506304074b37bcf4e2ebd7
Giulio Piancastelli <giulio.piancastelli@gmail.com>
2018-12-02 17:27:34 +0100

    Day 2

---
+++ src/main/kotlin/Day2.kt
@@ -0,0 +1,64 @@
+package aoc2018.day2
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val inputFile = File(ClassLoader.getSystemResource("input2.txt").file)
+    val lines = inputFile.readLines()
+
+    // solve first part
+    val checksum = findChecksum(lines)
+    // result: 4920
+    println(checksum)
+
+    // solve second part
+    val letters = findCommonLetters(lines)
+    // result: fonbwmjquwtapeyzikghtvdxl
+    println(letters)
+}
+
+fun findChecksum(lines: List<String>): Int {
+    val idList = mutableListOf<Set<Int>>()
+
+    for (line in lines) {
+        val letterMap = mutableMapOf<Char, Int>()
+        for (letter in line) {
+            letterMap.compute(letter) { _, v ->
+                if (v == null) 1 else v + 1
+            }
+        }
+        idList.add(letterMap.values.toSet())
+    }
+
+    val repetitionsOfTwo = idList.filter { 2 in it }
+    val repetitionsOfThree = idList.filter { 3 in it }
+
+    return repetitionsOfTwo.size * repetitionsOfThree.size
+}
+
+fun findCommonLetters(lines: List<String>): String? {
+    for (i in 0 until lines.size) {
+        for (j in i until lines.size) {
+            val id1 = lines[i]
+            val id2 = lines[j]
+            if (id1.length != id2.length) {
+                continue
+            }
+            var diffCount = 0
+            var diffIndex = 0
+            for (z in 0 until id1.length) {
+                if (id1[z] != id2[z]) {
+                    if (diffCount > 1) {
+                        break
+                    }
+                    diffCount++
+                    diffIndex = z
+                }
+            }
+            if (diffCount == 1) {
+                return id1.removeRange(diffIndex, diffIndex + 1)
+            }
+        }
+    }
+    return null
+}
